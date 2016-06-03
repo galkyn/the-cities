ff04223e2085bdf73e0b5a1b98e19464427b6520
q-ber <tehnomagix@mail.ru>
2016-06-03 14:11:21 +0200

    Добавил 3 функции: отправляет ответ на сервер, "пропуск хода", загрузка истории игры

--- js/common.js
+++ js/common.js
@@ -1,12 +1,40 @@
 $(function(){
    
+    function LoadGameLog() {
+        
+        $.post('ajax/game-log', function(data) {
+            data = jQuery.parseJSON(data);
+            
+            $('#gameLog').html('');
+            
+            $.each(data, function(key, value) {
+                $('#gameLog').append(' - ' + key + '<sup>' + value + ' км.</sup>');
+            });
+            
+        }); 
+        
+    }
+   
     $('#SendAnswerButton').click(function(){
         
-        $.post('ajax/ai-turn', { answer: $('#CityNameInput').val(), letter: $('#CheckLetterInput').val() }, function(data){
-	    //data = jQuery.parseJSON(data);    
-            $('#debug').html(data);  
+        $.post('ajax/step', { answer: $('#CityNameInput').val(), letter: $('#CheckLetterInput').val() }, function(data){
+	    data = jQuery.parseJSON(data);    
+            $('#debug').html(data.message + " : " + data.error);
+            LoadGameLog();
 	});
         
     });
+    
+    $('#SkipAnswerButton').click(function(){
+        
+        $.post('ajax/ai-turn', { answer: $('#CityNameInput').val(), letter: $('#CheckLetterInput').val() }, function(data){
+	    data = jQuery.parseJSON(data);    
+            $('#debug').html(data.message + " : " + data.error);
+            LoadGameLog();
+	});
+        
+    });
+    
+    LoadGameLog();
     
 });
