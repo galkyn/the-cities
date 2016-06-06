dcb8f6c9722dec85e59b72d157e9b3fd01a51b6f
q-ber <tehnomagix@mail.ru>
2016-06-06 20:51:37 +0200

    добавил вывод префикса для корректного формирования путей к скриптам и стилям

--- templates/page.header.template.php
+++ templates/page.header.template.php
@@ -6,10 +6,13 @@ header('Content-Type: text/html; charset=utf-8');
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css" />
-    <link type="text/css" rel="stylesheet" href="./css/common.css" />
-    <script type="text/javascript" src="./js/jquery-1.12.0.min.js"></script>
-    <script type="text/javascript" src="./js/bootstrap.js"></script>
-    <script type="text/javascript" src="./js/common.js"></script>
+    <link type="text/css" rel="stylesheet" href="<?=Route::$prefix?>css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="<?=Route::$prefix?>css/common.css" />
+    <script type="text/javascript">
+        var prefix = "<?=Route::$prefix?>";
+    </script>
+    <script type="text/javascript" src="<?=Route::$prefix?>js/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="<?=Route::$prefix?>js/bootstrap.js"></script>
+    <script type="text/javascript" src="<?=Route::$prefix?>js/common.js"></script>
 </head>
 <body>
