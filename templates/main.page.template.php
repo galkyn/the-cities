20ac5d6d92cde536d0c8684db5d0e8980e6995ca
q-ber <tehnomagix@mail.ru>
2016-06-06 20:50:41 +0200

    template для главной страницы - выбор режима игры

---
+++ templates/main.page.template.php
@@ -0,0 +1,16 @@
+<div class='wrapper'>
+    <div class='row'>
+        <div class='col-xs-12 col-sm-4'>
+            <div class='game-mode-title'>Одиночная игра</div>
+            <a href='<?=Route::$prefix?>game/single'><img src='<?=Route::$prefix?>images/icons/single.png' class='game-mode-icon'></a>
+        </div>
+        <div class='col-xs-12 col-sm-4'>
+            <div class='game-mode-title'>Игра с друзьями</div>
+            <a href='<?=Route::$prefix?>game/group'><img src='<?=Route::$prefix?>images/icons/group.png' class='game-mode-icon'></a>
+        </div>
+        <div class='col-xs-12 col-sm-4'>
+            <div class='game-mode-title'>Против всех</div>
+            <a href='<?=Route::$prefix?>game/world'><img src='<?=Route::$prefix?>images/icons/world.png' class='game-mode-icon'></a>
+        </div>
+    </div>
+</div>
