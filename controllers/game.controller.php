03c9e0b50f64318a6b44c74f75e54e7e8df4b87d
q-ber <tehnomagix@mail.ru>
2016-06-11 10:40:10 +0200

    изменил вызов шаблона

--- controllers/game.controller.php
+++ controllers/game.controller.php
@@ -3,12 +3,15 @@
 class GameController
 {
     
+    public static $gameMode;
+    
     public function InitializeWorldGame()
     {
+        self::$gameMode = 'world';
         
         include './templates/page.header.template.php';//Подключаем шаблон с заголовками сайта: скрипты, стили, тайтлы и т.д.
 	
-	include './templates/world.game.template.php';
+        include './templates/playground.template.php';
 	
 	include './templates/page.footer.template.php';
     }
