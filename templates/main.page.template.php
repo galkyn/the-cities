89e4a44daddaf0e5cb20463b610f1df03d73d224
q-ber <tehnomagix@mail.ru>
2016-06-11 10:39:52 +0200

    исправил ошибки верстки шаблона

--- templates/main.page.template.php
+++ templates/main.page.template.php
@@ -1,16 +1,24 @@
 <div class='wrapper'>
-    <div class='row'>
-        <div class='col-xs-12 col-sm-4'>
-            <div class='game-mode-title'>Одиночная игра</div>
-            <a href='<?=Route::$prefix?>game/single'><img src='<?=Route::$prefix?>images/icons/single.png' class='game-mode-icon'></a>
-        </div>
-        <div class='col-xs-12 col-sm-4'>
-            <div class='game-mode-title'>Игра с друзьями</div>
-            <a href='<?=Route::$prefix?>game/group'><img src='<?=Route::$prefix?>images/icons/group.png' class='game-mode-icon'></a>
-        </div>
-        <div class='col-xs-12 col-sm-4'>
-            <div class='game-mode-title'>Против всех</div>
-            <a href='<?=Route::$prefix?>game/world'><img src='<?=Route::$prefix?>images/icons/world.png' class='game-mode-icon'></a>
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class='col-xs-12 col-sm-4'>
+                <div class='game-mode-title'><nobr>Одиночная игра</nobr></div>
+                <div style='width: 100%; text-align: center;'>
+                    <a href='<?=Route::$prefix?>game/single'><img src='<?=Route::$prefix?>images/icons/single.png' class='game-mode-icon'></a>
+                </div>
+            </div>
+            <div class='col-xs-12 col-sm-4'>
+                <div class='game-mode-title'><nobr>Игра с друзьями</nobr></div>
+                <div style='width: 100%; text-align: center;'>
+                    <a href='<?=Route::$prefix?>game/group'><img src='<?=Route::$prefix?>images/icons/group.png' class='game-mode-icon'></a>
+                </div>
+            </div>
+            <div class='col-xs-12 col-sm-4'>
+                <div class='game-mode-title'><nobr>Против всех</nobr></div>
+                <div style='width: 100%; text-align: center;'>
+                    <a href='<?=Route::$prefix?>game/world'><img src='<?=Route::$prefix?>images/icons/world.png' class='game-mode-icon'></a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
