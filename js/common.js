8333987c29c0bbc33cd7a71fc0848de8f0e33251
q-ber <tehnomagix@mail.ru>
2016-06-11 10:40:37 +0200

    промежуточное решение, будете переделано

--- js/common.js
+++ js/common.js
@@ -1,35 +1,53 @@
 $(function(){
    
+    function CheckLetter() {
+	
+	if ($('#CheckLetterInput').val() == '_NA_') {
+	    return true;
+	} else {
+	    var UserLetter = $('#CityNameInput').val().charAt(0);
+	    if (UserLetter == $('#CheckLetterInput').val()) {
+		return true;
+	    } else {
+		return false;
+	    }
+	}
+	
+	
+    }   
+   
     function LoadGameLog() {
         
-        $.post(prefix + 'ajax/game-log', function(data) {
-            data = jQuery.parseJSON(data);
-            
-            $('#gameLog').html('');
-            
-            $.each(data, function(key, value) {
-                $('#gameLog').append(' - ' + key + '<sup>' + value + ' км.</sup>');
-            });
-            
-        }); 
+	    $.post(prefix + 'ajax/game-log', { mode: gameMode }, function(data) {
+		data = jQuery.parseJSON(data);
+		
+		$('#gameLog').html('');
+		var str = '';
+		
+		$.each(data, function(key, value) { str += ' - ' + key + '<sup>' + value + ' км.</sup>'; });
+		
+		$('#gameLog').html(str.substring(3));
+		
+	    }); 
         
     }
    
     $('#SendAnswerButton').click(function(){
         
-        $.post(prefix + 'ajax/step', { answer: $('#CityNameInput').val(), letter: $('#CheckLetterInput').val() }, function(data){
-	    data = jQuery.parseJSON(data);
-            $('#CityNameInput').val(data.letter);
-            $('#CheckLetterInput').val(data.letter);
-            $('#debug').html(data.message + " : " + data.error);
-            LoadGameLog();
-	});
-        
+	if (CheckLetter) {
+	    $.post(prefix + 'ajax/step', { mode: gameMode, answer: $('#CityNameInput').val(), letter: $('#CheckLetterInput').val() }, function(data){
+		data = jQuery.parseJSON(data);
+		$('#CityNameInput').val(data.letter);
+		$('#CheckLetterInput').val(data.letter);
+		$('#debug').html(data.message + " : " + data.error);
+		LoadGameLog();
+	    });
+	}
     });
     
     $('#SkipAnswerButton').click(function(){
         
-        $.post(prefix + 'ajax/ai-turn', { answer: $('#CityNameInput').val(), letter: $('#CheckLetterInput').val() }, function(data){
+        $.post(prefix + 'ajax/ai-turn', { mode: gameMode, answer: $('#CityNameInput').val(), letter: $('#CheckLetterInput').val() }, function(data){
 	    data = jQuery.parseJSON(data);
             $('#CityNameInput').val(data.letter);
             $('#CheckLetterInput').val(data.letter);
