c09900dac5944d8e22aadfd68d5d3afdab05dc83
q-ber <tehnomagix@mail.ru>
2016-06-11 10:38:44 +0200

    поправил ошибки

--- templates/page.header.template.php
+++ templates/page.header.template.php
@@ -10,8 +10,10 @@ header('Content-Type: text/html; charset=utf-8');
     <link type="text/css" rel="stylesheet" href="<?=Route::$prefix?>css/common.css" />
     <script type="text/javascript">
         var prefix = "<?=Route::$prefix?>";
+        <?if(class_exists('GameController')){?>var gameMode = "<?=GameController::$gameMode?>";<?}?>
     </script>
     <script type="text/javascript" src="<?=Route::$prefix?>js/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="<?=Route::$prefix?>js/jquery.countdown360.min.js"></script>
     <script type="text/javascript" src="<?=Route::$prefix?>js/bootstrap.js"></script>
     <script type="text/javascript" src="<?=Route::$prefix?>js/common.js"></script>
 </head>
