684d122c54f92c11be4650220769729288d0fc51
q-ber <tehnomagix@mail.ru>
2016-06-12 14:22:35 +0200

    поправил верстку

--- templates/main.page.template.php
+++ templates/main.page.template.php
@@ -2,19 +2,19 @@
     <div class='container-fluid'>
         <div class='row'>
             <div class='col-xs-12 col-sm-4'>
-                <div class='game-mode-title'><nobr>Одиночная игра</nobr></div>
+                <div class='game-mode-title' style='color: #333; text-align: center; margin-bottom: 5%;'><nobr>Одиночная игра</nobr></div>
                 <div style='width: 100%; text-align: center;'>
                     <a href='<?=Route::$prefix?>game/single'><img src='<?=Route::$prefix?>images/icons/single.png' class='game-mode-icon'></a>
                 </div>
             </div>
             <div class='col-xs-12 col-sm-4'>
-                <div class='game-mode-title'><nobr>Игра с друзьями</nobr></div>
+                <div class='game-mode-title' style='color: #333; text-align: center; margin-bottom: 5%;'><nobr>Игра с друзьями</nobr></div>
                 <div style='width: 100%; text-align: center;'>
                     <a href='<?=Route::$prefix?>game/group'><img src='<?=Route::$prefix?>images/icons/group.png' class='game-mode-icon'></a>
                 </div>
             </div>
             <div class='col-xs-12 col-sm-4'>
-                <div class='game-mode-title'><nobr>Против всех</nobr></div>
+                <div class='game-mode-title' style='color: #333; text-align: center; margin-bottom: 5%;'><nobr>Против всех</nobr></div>
                 <div style='width: 100%; text-align: center;'>
                     <a href='<?=Route::$prefix?>game/world'><img src='<?=Route::$prefix?>images/icons/world.png' class='game-mode-icon'></a>
                 </div>
