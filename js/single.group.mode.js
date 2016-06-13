8c31ca9bc6d90aa4245dcb8fd92c0bd82a969cd2
q-ber <tehnomagix@mail.ru>
2016-06-13 12:23:13 +0200

    написал функции: "отобразить список игроков", "добавить игрока", "добавить бота", "удалить игрока или бота из листа"

--- js/single.group.mode.js
+++ js/single.group.mode.js
@@ -3,6 +3,45 @@ var scoreTable = {};
 
 var CitiesStack = {};
 
+var BotNames = ["Bashful Bot", "Doc Bot", "Dopey Bot", "Grumpy Bot", "Happy Bot", "Sleepy Bot", "Sneezy Bot"]; 
+var BotCounter = 0;
+
+function ShowPlayersList()
+{
+     $('#PlayersListBox').html('');
+        
+    for(name in scoreTable)
+    {
+        $('#PlayersListBox').append("<tr><td width='8%' align='left' style='padding:3px;'><img src='" + prefix + "/images/icons/" + scoreTable[name].player_type + ".png' width='24' height='24'></td><td>" + name + "</td><td width='20%' align='center'>" + scoreTable[name].score + "</td><td width='8%' align='right' style='padding:3px;'><img src='" + prefix + "/images/icons/delete.png' width='24' height='24' style='cursor:pointer;' onClick='DetelePlayerFromList(\"" + name + "\");'></td></tr>");
+    }
+}
+ 
+function AddBotToList()
+{
+    if (BotCounter < BotNames.length) {
+        var playerName = BotNames[BotCounter];
+        
+        if (playerName in scoreTable) {
+            BotCounter++;
+            AddBotToList();
+        } else {
+            scoreTable[playerName] = { player_type:'ai', score:0 };
+            BotCounter++;
+            ShowPlayersList();
+        }
+    } else {
+        alert("Пожалуй хватит! Пусть и для людей место останется.");
+    } 
+}
+    
+function DetelePlayerFromList(playerName)
+{
+    delete scoreTable[playerName];
+    BotCounter = 0;
+    ShowPlayersList();
+}
+
+
 $(function(){
     
     if (gameMode == 'single') {
@@ -13,21 +52,14 @@ $(function(){
     $('#AddNewPlayerToScoreListButton').click(function() {
         var playerName = $('#PlayerName').val();
         
-        scoreTable[playerName] = { player_type:'human', score:0 }
-        
-        ShowPlayersList();
+        if (!(playerName in scoreTable)) {
+            scoreTable[playerName] = { player_type:'human', score:0 };
+            ShowPlayersList();
+        } else {
+            alert("Игрок с именем " + playerName + " уже зарегистрирован.");
+        }
         
     });
     
-    
-    function ShowPlayersList()
-    {
-        $('#PlayersListDiv').html('');
-        
-        for(name in scoreTable)
-        {
-            $('#PlayersListDiv').append(name + ' - ' + scoreTable[name].player_type + ' - ' + scoreTable[name].score + '<br>');
-        }
-    }
     
 });
