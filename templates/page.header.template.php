d0d058a3bd433a6541cf4027f07b937bbab397ac
q-ber <tehnomagix@mail.ru>
2016-06-17 00:35:33 +0200

    заменил подключаемый jquery-плагин обратного отсчета, этот пободрее

--- templates/page.header.template.php
+++ templates/page.header.template.php
@@ -8,12 +8,13 @@ header('Content-Type: text/html; charset=utf-8');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="text/css" rel="stylesheet" href="<?=Route::$prefix?>css/bootstrap.min.css" />
     <link type="text/css" rel="stylesheet" href="<?=Route::$prefix?>css/common.css" />
+    <link type="text/css" rel="stylesheet" href="<?=Route::$prefix?>css/TimeCircles.css" />
     <script type="text/javascript">
         var prefix = "<?=Route::$prefix?>";
         <?if(class_exists('GameController')){?>var gameMode = "<?=GameController::$gameMode?>";<?}?>
     </script>
     <script type="text/javascript" src="<?=Route::$prefix?>js/jquery-1.12.0.min.js"></script>
-    <script type="text/javascript" src="<?=Route::$prefix?>js/jquery.countdown360.min.js"></script>
+    <script type="text/javascript" src="<?=Route::$prefix?>js/TimeCircles.js"></script>
     <script type="text/javascript" src="<?=Route::$prefix?>js/bootstrap.js"></script>
     <script type="text/javascript" src="<?=Route::$prefix?>js/common.js"></script>
     <?if((class_exists('GameController')) && ((GameController::$gameMode == 'group') || (GameController::$gameMode == 'single'))){?><script type="text/javascript" src="<?=Route::$prefix?>js/single.group.mode.js"></script><?}?>
