7717c9f896bb0b42921c23a489a0ddb47c39ee29
q-ber <tehnomagix@mail.ru>
2016-06-25 14:17:21 +0200

    закоментил режим отображения ошибок

--- engine.php
+++ engine.php
@@ -3,12 +3,15 @@
 //echo ini_get('display_errors');
 
 if (!ini_get('display_errors')) {
-    ini_set('display_errors', '1');
+    ini_set('display_errors', '0');
 }
 
 //echo ini_get('display_errors');
 
-error_reporting(E_ALL);
+error_reporting(0);
+
+//error_reporting(E_ALL);
+//error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 require_once './classes/dbase.class.php';
 require_once './classes/common.class.php';
